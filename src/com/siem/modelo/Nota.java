a31cf67ca858472b68dfadac70365f4fe6b52079
alejandro <alejandro@AlexUbuntu>
2014-01-31 00:48:49 -0600

    -Borrado comentarios superiores feos de NetBeans. -Corrigiendo errores de los metodos ToString de los pojos Nota y Supervisor.

--- src/com/siem/modelo/Nota.java
+++ src/com/siem/modelo/Nota.java
@@ -1,8 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 
 package com.siem.modelo;
 
@@ -46,7 +41,7 @@ public class Nota implements Serializable {
 
     @Override
     public String toString() {
-        return String.format("%s %s %s", id, nombre);
+        return String.format("%s %s ", id, nombre);
     }
     
     
--- src/com/siem/modelo/Supervisor.java
+++ src/com/siem/modelo/Supervisor.java
@@ -1,9 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
 package com.siem.modelo;
 
 import java.io.Serializable;
@@ -48,7 +42,7 @@ public class Supervisor implements Serializable {
 
     @Override
     public String toString() {
-        return String.format("%s %s %s", id, nombre);
+        return String.format("%s %s", id, nombre);
     }
 
    
