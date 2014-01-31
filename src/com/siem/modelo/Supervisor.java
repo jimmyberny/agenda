17fb34d97629561ee3e772ac5e9c09766076cbb0
alejandro <alejandro@AlexUbuntu>
2014-01-31 00:13:13 -0600

    Favor de revisar y mandar comentarios para mejorar

---
+++ src/com/siem/modelo/Supervisor.java
@@ -0,0 +1,58 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package com.siem.modelo;
+
+import java.io.Serializable;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import org.hibernate.annotations.GenericGenerator;
+
+
+@Entity
+@Table(name = "supervisor")
+public class Supervisor implements Serializable {
+    
+    private static final long serialVersionUID = 4L;
+    
+    @Id
+    @GeneratedValue(generator = "uuid")
+    @GenericGenerator(name = "uuid", strategy = "uuid2")
+    private String id;
+    @Column
+    private String nombre;
+
+    public String getId() {
+        return id;
+    } 
+
+    public String getNombre() {
+        return nombre;
+    }
+    
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+        
+    
+    public void setId(String id) {
+        this.id = id;
+    }
+
+
+    @Override
+    public String toString() {
+        return String.format("%s %s %s", id, nombre);
+    }
+
+   
+    
+    
+   
+}
